4818846c8572caf61fa55ea964b06caa28eaf545
Nobbele <realnobbele@gmail.com>
2019-12-25 00:04:29 +0100

    POSIX-compliant and shellcheck sh script

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$(id -ru)" -x polybar >/dev/null; do sleep 1; done
 
 NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+export NETWORK_INTERFACE
 polybar bar &
 
 echo "Started Polybar"
